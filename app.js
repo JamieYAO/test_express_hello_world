cc44d1825554ccbae68fa1c9c8c9050aef7b3172
jamie <272915189@qq.com>
2016-08-16 09:33:25 +0800

    Add some bash script and router.

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
 
+app.use('/res', express.static('views'));
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/v1/test-api', function (req, res) {
+  res.send({ ret: 1000 });
+});
+
+app.get('/res/:id', function (req, res) {
+  var id = req.params.id;
+  var query_id = req.query.id;
+  res.send(id);
+});
+
+app.get('/home', function (req, res) {
+  res.render('home', { title: 'Hey', message: 'Hello there!'});
 });
 
 app.listen(3000, function () {
