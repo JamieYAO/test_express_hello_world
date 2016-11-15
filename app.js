07c7109e9b252859c597399a36603b0a6a5c196c
jamie <272915189@qq.com>
2016-11-15 17:51:05 +0800

    Added comments.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.use('/res', express.static('views'));
+app.use('/res', express.static('views')); // Set static path.
 
-app.set('views', './views');
-app.set('view engine', 'pug');
+app.set('views', './views'); // Set views path.
+app.set('view engine', 'pug'); // Set view engine.
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { // Set root router.
   res.send('Hello World!');
 });
 
-var appRouter = express.Router();
+var appRouter = express.Router(); // Set one router.
 
 appRouter.get('/v1/test-api', function (req, res) {
   res.send({ ret: 1000 });
@@ -23,10 +23,11 @@ appRouter.get('/res/:id', function (req, res) {
 });
 
 appRouter.get('/home', function (req, res) {
-  res.render('home', { title: 'Hey', message: 'Hello there!'});
+  var title = req.query.title;
+  res.render('home', { title: title, message: 'Hello there!'});
 });
 
-app.use('/base', appRouter);
+app.use('/base', appRouter); // Use router.
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
